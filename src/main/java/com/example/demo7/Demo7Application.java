26205a30cea5b93aea999b2e6a440ee85867a006
elenapoker888@gmail.com <elenapoker888@gmail.com>
2020-01-08 19:22:35 +0400

    sql

--- src/main/java/com/example/demo7/Demo7Application.java
+++ src/main/java/com/example/demo7/Demo7Application.java
@@ -4,27 +4,35 @@ import org.apache.ibatis.jdbc.ScriptRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManager;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @SpringBootApplication
 
 
 public class Demo7Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+//        Class.forName("org.hsqldb.jdbc.JDBCDriver");
+//
+//        String script = "schema.sql";
+//        try {
+//            new ScriptRunner(DriverManager.getConnection(
+//                    "jdbc:hsqldb:file:testDB", "sa", "123"))
+//                    .runScript(new BufferedReader(new FileReader(script)));
+//        } catch (Exception e) {
+//            System.err.println(e);
+//        }
+
+
         SpringApplication.run(Demo7Application.class, args);
 
-        String script = "init.sql";
-        try {
-            Class.forName("org.hsqldb.jdbc.JDBCDriver");
-            new ScriptRunner(DriverManager.getConnection(
-                    "jdbc:hsqldb:file:testDB", "sa", "123"))
-                    .runScript(new BufferedReader(new FileReader(script)));
-        } catch (Exception e) {
-            System.err.println(e);
-        }
+
     }
 
 }
